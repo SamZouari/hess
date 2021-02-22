93c82c70f66de757900eba3a8ecfdbe72f8e06fb
SamZouari <samuel.zouari@yahoo.fr>
2021-02-22 10:39:04 +0100

    mise à jour du script pour la simulation de données HESS J1745-290

--- notebooks0.18.2/hap-fr/.ipynb_checkpoints/script_simu_long-checkpoint.py
+++ notebooks0.18.2/hap-fr/.ipynb_checkpoints/script_simu_long-checkpoint.py
@@ -63,8 +63,9 @@ pathmo.mkdir(exist_ok=True)
 # but it will generally require that the cutoff of the DE be frozen and set to infinity (lambda = 0)
 
 model_name = pathmo/"models_template_2cutoff.yaml" 
+#model_name = path/"3Dspectra/2amps_2indexes_1cutoff/joint_model_fitted.yaml"
 
-pathres = Path(path/"simu_variable")
+pathres = Path(path/"simu_variable/year_per_year_sensitivity/inprocess")
 pathres.mkdir(exist_ok=True)
 
 ## Geometry
@@ -106,19 +107,19 @@ modelGC,modelG09, modeldiff= Models.read(model_name)
 # Setting which parameters are free, and their "baseline" value
 
 modelGC.parameters["amplitude"].frozen = False
-modelGC.parameters["amplitude"].value = 2.12e-12 #2.12
+modelGC.parameters["amplitude"].value = 2.14e-12 #2.12
 
 modelGC.parameters["index"].frozen = True
-modelGC.parameters["index"].value = 1.77
+modelGC.parameters["index"].value = 1.835
 
 modelGC.spectral_model.parameters['lambda_'].frozen = True
-modelGC.spectral_model.parameters['lambda_'].value = 1/5.4
+modelGC.spectral_model.parameters['lambda_'].value = 1/6.381
 
 modeldiff.parameters["amplitude"].frozen = False
-modeldiff.parameters["amplitude"].value = 6.09e-12
+modeldiff.parameters["amplitude"].value = 5.83e-12
 
 modeldiff.parameters["index"].frozen = True
-modeldiff.parameters["index"].value = 2.24
+modeldiff.parameters["index"].value = 2.243
 
 modeldiff.spectral_model.parameters['lambda_'].frozen = True
 modeldiff.spectral_model.parameters['lambda_'].value = 0.0
@@ -151,47 +152,63 @@ def fit_dataset(mapdataset):
 emin = 1.0*u.TeV
 emax = 10*u.TeV
 
-GCflux_distribution = []
-DEflux_distribution = []
+#GCflux_distribution = []
+#DEflux_distribution = []
 
 
 # à ne définir qu'une fois, puis on rajoute des éléments 
 #en prenant soin de laisser k augmenter sans remise à zéro dans les appels successifs de la cellule suivante
 
 year = int(input("which year : "))
-iterations = int(input('how many iterations : '))
+iterations = int(input('how many iterations : ')) #should be 50, it breaks around 47 usually, might depend on the year
+adding = input("are we adding simulated runs ? (y/n) ")
 
-k = 0
 
+if adding=='y' or adding=="yes":
+    list = []
+    with open(pathres/f'GC_fluxes_simu_{year}.txt', 'r') as GCfile:
+        for line in GCFile:
+            x = line[:-1]
+            list.append(x)
+            n = len(list)
+            k = n
+            iterations = k + iterations
+else:
+    k = 0
+
+
+with open(pathres/f'GC_fluxes_simu_{year}.txt', 'a+') as GCfile:
+    GCfile.write("")
+with open(pathres/f'DE_fluxes_simu_{year}.txt', 'a+') as DEfile:
+    DEfile.write("")
+        
 while k <  iterations :
-    #ça fonctionne comme ça, tous les modèles existent indépendamment des datasets donc on peut continuer à 
+            #ça fonctionne comme ça, tous les modèles existent indépendamment des datasets donc on peut continuer à 
     dataset = MapDataset.read(pathma/f"map_{year}.fits")
-    
+
     dataset.models =  [modelGC.copy(),modelG09.copy(),modeldiff.copy()]     
     dataset.fake(k) 
-        
-    # on remet les modèles
+
+            # on remet les modèles
     dataset.mask_fit = mask_fit
     bkg_model = FoVBackgroundModel(dataset_name=f"map_{year}")
-        
+
     dataset.models =  [modelGCnaive.copy(), modelG09.copy(), modeldiffnaive.copy(),bkg_model]
 
-        # on fait le fit
+                # on fait le fit
     table, result = fit_dataset(dataset)
-        
+
     diffuse_flux = dataset.models[2].spectral_model.integral(emin, emax)
     GC_flux = dataset.models[0].spectral_model.integral(emin, emax)
-        
-    GCflux_distribution.append(GC_flux.value)
-    DEflux_distribution.append(diffuse_flux.value)
-        
+
+            #GCflux_distribution.append(GC_flux.value)
+            #DEflux_distribution.append(diffuse_flux.value)
+    with open(pathres/f'GC_fluxes_simu_{year}.txt', 'a+') as GCfile:
+        GCfile.write('%s\n' % GC_flux.value)
+    with open(pathres/f'DE_fluxes_simu_{year}.txt', 'a+') as DEfile:
+        DEfile.write('%s\n' % diffuse_flux.value)
+
     k = k+1
     
 
-with open(pathres/f'GC_fluxes_simu_{year}.txt', 'w') as filehandle:
-    for listitem in GCflux_distribution:
-        filehandle.write('%s\n' % listitem)
 
-with open(pathres/f'DE_fluxes_simu_{year}.txt', 'w') as filehandle:
-    for listitem in DEflux_distribution:
-        filehandle.write('%s\n' % listitem)
